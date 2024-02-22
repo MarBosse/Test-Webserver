3d15f76bed5f6ae1113286c6437a99e44cb9c8a8
Marcel Bosse <marcel.bosse@tensora.co>
2024-02-22 11:18:56 +0100

    Added test audio case

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import logging
 
+from pydub import AudioSegment
 from fastapi import FastAPI
 from dotenv import load_dotenv
 from contextlib import asynccontextmanager
@@ -26,3 +27,10 @@ app = FastAPI(lifespan=lifespan)
 def health() -> dict:
     logging.info("Health endpoint processed a request.")
     return {"info": "I'm healthy"}
+
+@app.get("/api/test")
+def health() -> dict:
+    audio = AudioSegment.from_file("test.m4a", format="m4a")
+    new_filename = "1_test.mp3"
+    audio.export(new_filename, format="mp3", bitrate="64k")
+    return {"info": "I'm done"}
